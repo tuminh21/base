8df323ad98bc2ebde15a581f079f9a4e60e77fe1
tuminh21 <tunguyenminh210502@gmail.com>
2024-04-26 22:50:42 +0700

    base

---
+++ pca10056/s140/ses/Output/Release/Obj/ble_app_uart_pca10056_s140/queue.d
@@ -0,0 +1,5 @@
+Output/Release/Obj/ble_app_uart_pca10056_s140/queue.o: \
+ D\:\hope\DeviceDownload\nRF5_SDK_15.2.0_9412b96\examples\ble_peripheral\hope\Libra\queue.c \
+ D\:\hope\DeviceDownload\nRF5_SDK_15.2.0_9412b96\examples\ble_peripheral\hope\Libra\queue.h \
+ D\:/SEGGER/include/stdint.h D\:/SEGGER/include/string.h \
+ D\:/SEGGER/include/__crossworks.h
